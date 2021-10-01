406c00389bf0383fa8c3c5bb812c676e98ab11a9
Oscar Alberto Guiamba <oscarguiamba.a@gmail.com>
2021-10-01 11:03:08 -0700

    INICIANDO U  PROJECTO
    # django-admin startproject lojaPy ==> Nome do projecto

---
+++ detalhes.py
@@ -0,0 +1,16 @@
+# CRIANDO O AMBIENTE VIRTUAL ==>
+                              # python -m venv pystore ==> ambiente virtual
+# ==============================================================================#
+
+#INICIANDO U  PROJECTO ==>
+                        # django-admin startproject lojaPy ==> Nome do projecto
+# ==============================================================================#
+
+
+#LIGANDO O SERVIDOR ==>
+# (pystore) PS C:\Users\Oscar AlbertoGuiamba\Documents\GitHub\site\ ==>
+                                                                 # cd lojapy> 
+                                                                # python manage.py runserver
+
+# ==============================================================================#
+
