4f9b3f50e80e7f55da573bce7b120df96b622396
Oscar Alberto Guiamba <oscarguiamba.a@gmail.com>
2021-10-03 08:26:51 -0700

    personalizar
    personalizando templats

--- detalhes.py
+++ detalhes.py
@@ -41,3 +41,23 @@
 #Database Service: ==> postgresql-x64-13
 #Command Line Tools Installation Directory: ==> C:\Program Files\PostgreSQL\13
 #pgAdmin4 Installation Directory: ==> C:\Program Files\PostgreSQL\13\pgAdmin 4
+
+
+#===============================================================================#
+
+  #<form class="form-group" action="{% url 'cart:add' product.id %}" method="post">
+                #<p class="form-inline">
+                   # {{ form.quantity.label_tag }}
+                    #{% render_field form.quantity class+="form-control ml-sm-3" %}
+                    #{{ form.override }}
+               # </p>
+               # {% csrf_token %}
+               # <input class="btn btn-success" type="submit" value="Adicionar ao Carrinho">
+            #</form> -->
+#=================================================================================#
+
+
+
+# Media
+#MEDIA_URL = "/media/"
+#MEDIA_ROOT = BASE_DIR / "media"
